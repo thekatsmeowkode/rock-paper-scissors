566985e176b999269e15d9cef8a4ad9ba3f42e99
Kat M <kmunson13@gmail.com>
2022-08-17 09:15:05 -0500

    Fix all bugs, integrate all functions into cohesive whole, remove comments

--- rps.js
+++ rps.js
@@ -1,42 +1,39 @@
 //define 3 possible choice variables
 let choices = ['rock', 'paper', 'scissors'];
 
-/*let playerSelection = prompt('Choose paper, rock, or scissors', 'default').toUpperCase();*/
+//variables to keep scores
+ let playerScore = 0
+ let computerScore = 0
 
-/*let playerSelection = 'paper'*/
-
-//debug helper
-/*console.log(playerSelection)*/
-
-//beginning of game loop
-for (let i = 1; i < 6; i++) {
-  //variables to keep scores
-  let playerScore = 0
-  let computerScore = 0
-  //make a way to generate random  computerChoice
+//make a way to generate random  computerChoice
   var getComputerChoice = choices[Math.floor(Math.random() * choices.length)];
-  //define parameter for playround function using randomly generated computerChoice
-  let computerSelection = getComputerChoice
-  console.log(`Computer chose ${computerSelection}`)
-  let playerSelection = 'paper'
-  console.log(`Player chose paper`)
-  //if else statement to determine winner
+
+//if else statement to determine winner
   function playround(playerSelection, computerSelection) {
-    if (computerSelection === 'rock' && playerSelection === 'paper') { return playerScore++; console.log("You win") }
-    else if (computerSelection === 'rock' && playerSelection === 'scissors') { return computerScore++; console.log("You lose") }
-    else if (computerSelection === 'rock' && playerSelection === 'rock') { return "Draw" }
-    else if (computerSelection === 'scissors' && playerSelection === 'paper') { return computerScore++; console.log("You lose") }
-    else if (computerSelection === 'scissors' && playerSelection === 'rock') { return playerScore++; console.log("You win") }
-    else if (computerSelection === 'scissors' && playerSelection === 'scissors') { return "Draw" }
-    else if (computerSelection === 'paper' && playerSelection === 'scissors') { return playerScore++; console.log("You win") }
-    else if (computerSelection === 'paper' && playerSelection === 'rock') { return computerScore++; console.log("You lose") }
-    else if (computerSelection === 'paper' && playerSelection === 'paper') { return 'Draw' }
+    if (computerSelection === 'rock' && playerSelection === 'paper') { return playerScore++, console.log("You win")}
+    else if (computerSelection === 'rock' && playerSelection === 'scissors') { return computerScore++, console.log("You lose")}
+    else if (computerSelection === 'rock' && playerSelection === 'rock') { return console.log("Draw" )}
+    else if (computerSelection === 'scissors' && playerSelection === 'paper') { return computerScore++, console.log("You lose")}
+    else if (computerSelection === 'scissors' && playerSelection === 'rock') { return playerScore++, console.log("You win")}
+    else if (computerSelection === 'scissors' && playerSelection === 'scissors') { return console.log("Draw") }
+    else if (computerSelection === 'paper' && playerSelection === 'scissors') { return playerScore++, console.log("You win")}
+    else if (computerSelection === 'paper' && playerSelection === 'rock') { return computerScore++, console.log("You lose")}
+    else if (computerSelection === 'paper' && playerSelection === 'paper') { return console.log('Draw') }
     else { return 'invalid' };
   }
-  //call playround function
+
+let game = () => {
+  for (let i=0; i < 5; i++)
+{ var getComputerChoice = choices[Math.floor(Math.random() * choices.length)];
+  let computerSelection = getComputerChoice
+  let playerSelection = prompt("Choose Paper, Rock, or Scissors", "Paper, rock, or scissors").toLowerCase()
+  console.log(`Player chose ${playerSelection}`)
+  console.log(`Computer chose ${computerSelection}`)
   playround(playerSelection, computerSelection);
+}
+if (playerScore > computerScore) {return "Player wins"}
+else if (playerScore < computerScore) {return "Computer wins"}
+else {return "There was a tie"}
+}
 
-  console.log(`Player score: ${playerScore}`)
-
-  console.log(`Computer score: ${computerScore}`)
-  }
+console.log(game())
